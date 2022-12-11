0a6ade4dddd0af0236c86a6cfaa76abbce510754
richardqluo <richardqluo@yahoo.com>
2022-12-11 15:49:08 -0500

    Update OrderBook.cpp

--- OrderBook.cpp
+++ OrderBook.cpp
@@ -14,6 +14,7 @@ using namespace std;
 
 
 struct Order{
+public:
     unsigned long long id;
     unsigned long time;
     string symbol;
@@ -21,7 +22,7 @@ struct Order{
     char side;
     unsigned long long shares;
     double price;
-    Order(unsigned long long i, unsigned int tm, string sb, char tp, char sd, int sh, int p):id(i),time(tm),symbol(sb),type(tp),side(sd),shares(sh),price(p){}
+    Order(unsigned long long i, unsigned long tm, string sb, char tp, char sd, unsigned long long sh, double p) : id(i),time(tm),symbol(sb),type(tp),side(sd),shares(sh),price(p){};
 };
 
 class Matcher{
@@ -30,13 +31,13 @@ private:
     unordered_map<string,map<double,queue<shared_ptr<Order>>>> bids;
     unordered_map<unsigned long long,shared_ptr<Order>> orders;
 
-    
+
 public:
-    void load(std::vector<Order>& O){
-        for(auto o : O){
+    void load(vector<Order>& O){
+        for(Order o : O){
             auto spo = make_shared<Order>(o.id,o.time,o.symbol,o.type,o.side,o.price);
             orders[o.id] = spo;
-            
+
             if(o.side == 'B'){
                 auto itb = bids.find(o.symbol);
                 if(itb != bids.end()){
@@ -51,7 +52,7 @@ public:
                 }else{
                     queue<shared_ptr<Order>> oq;
                     oq.push(spo);
-                    map<double,queue<shared_ptr<Order>> bm;
+                    map<double,queue<shared_ptr<Order>>> bm;
                     bm.insert(make_pair(o.price, oq));
                     bids.insert(make_pair(o.symbol, bm));
                 }
@@ -69,16 +70,16 @@ public:
                 }else{
                     queue<shared_ptr<Order>> oq;
                     oq.push(spo);
-                    map<double,queue<shared_ptr<Order>> am;
+                    map<double,queue<shared_ptr<Order>>,greater<double>> am;
                     am.insert(make_pair(o.price, oq));
                     asks.insert(make_pair(o.symbol, am));
                 }
-            } 
-        } 
+            }
+        }
     };
     vector<int> match(unsigned long T, string S){
         vector<int> r;
-        
+
         auto itb = bids.find(S);
         if(itb == bids.end()){
             return r;
@@ -87,35 +88,35 @@ public:
         if(ita == asks.end()){
             return r;
         }
-        
+
         double bestBidPrice = itb->second.begin()->first;
         double bestAskPrice = ita->second.begin()->first;
-        
+
         while(bestBidPrice>=bestAskPrice){
             auto spa = ita->second.begin()->second.front();
             auto spb = itb->second.begin()->second.front();
-                
+
             int size = spb->shares - spa->shares;
             if(size == 0){
                 itb->second.begin()->second.pop();
-                ita->second.begin()->second.pop()           
+                ita->second.begin()->second.pop();
             }else if(size > 0){
                 spb->shares = size;
-                ita->second.begin()->second.pop()
+                ita->second.begin()->second.pop();
             }else if(size < 0){
                 spa->shares = -size;
-                itb->second.begin()->second.pop()
+                itb->second.begin()->second.pop();
             }
-            if(ita->second.begin()->second.empty(){
+            if(ita->second.begin()->second.empty()){
                 ita->second.erase(bestAskPrice);
                 if(ita->second.empty()){
-                    asks.erase(ita); 
+                    asks.erase(ita);
                 }
             }
-            if(itb->second.begin()->second.empty(){
+            if(itb->second.begin()->second.empty()){
                 itb->second.erase(bestBidPrice);
                 if(itb->second.empty()){
-                    bids.erase(itb); 
+                    bids.erase(itb);
                 }
             }
             if(asks.empty() || bids.empty()){
@@ -124,11 +125,12 @@ public:
             bestBidPrice = itb->second.begin()->first;
             bestAskPrice = ita->second.begin()->first;
         }
-        
+
         //cout << it->first << " : " << it->second << '\n';
         return r;
     }
 };
+
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
