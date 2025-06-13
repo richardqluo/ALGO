5f2bedef6d0fa25a30c6a1bc1f7dc3852374cb04
richardqluo <richardqluo@yahoo.com>
2025-06-13 13:08:33 -0400

    Update TFRingBuffer.h

--- TFRingBuffer.h
+++ TFRingBuffer.h
@@ -7,9 +7,9 @@ using namespace std;
 template <typename T>
 
 class TFRingBuffer {
-    T *m_buffer;
-    atomic<size_t> m_head;
-    atomic<size_t> m_tail;
+    T* m_buffer;
+    atomic<size_t> m_write;
+    atomic<size_t> m_read;
     const size_t m_size; //typedef unsigned long size_t;
 
     size_t next(size_t current) {
@@ -18,34 +18,55 @@ class TFRingBuffer {
 
 public:
 
-    TFRingBuffer(const size_t size) : m_size(size), m_head(0), m_tail(0) {
+    TFRingBuffer(const size_t size) : m_size(size), m_write(0), m_read(0) {
         m_buffer = new T[size];//fixed size
     }
 
     virtual ~TFRingBuffer() {
         delete[] m_buffer;
     }
-    //consume m_tail and produce m_head
+    //single write read thread
+    bool empty(){
+        return m_write == m_read;
+    }
+    bool full(){
+        return m_read == next(m_write);
+    }
     bool push(const T &object) {
-        size_t head = m_head.load(memory_order_relaxed);//only thread to produce m_head, surely latest, so no need sync
+        if(full()}
+            return false;
+        m_write = next(m_write);
+        m_buffer[m_write] = object;
+        return true;
+    }
+    bool pop(T &object) {
+        if(empty())
+            return false;
+        m_read = next(m_read);
+        object = m_buffer[m_read];
+        return true;
+    }
+    //multiple write read threads
+    bool push(const T &object) {
+        size_t head = m_write.load(memory_order_relaxed);//only thread to produce m_write, surely latest, so no need sync
         size_t nextHead = next(head);
-        if (nextHead == m_tail.load(memory_order_acquire)) {//read=write+1 full
+        if (nextHead == m_read.load(memory_order_acquire)) {//read=write+1 full
             return false;
         }
         m_buffer[head] = object;//before store to guaranteed the save is visible
-        m_head.store(nextHead, memory_order_release);
+        m_write.store(nextHead, memory_order_release);
 
         return true;
     }
-    //consume m_head and produce m_tail
+    
     bool pop(T &object) {
-        size_t tail = m_tail.load(memory_order_relaxed);
-        if (tail == m_head.load(memory_order_acquire)) {//read=write empty
+        size_t tail = m_read.load(memory_order_relaxed);
+        if (tail == m_write.load(memory_order_acquire)) {//read=write empty
             return false;
         }
 
         object = m_buffer[tail];
-        m_tail.store(next(tail), memory_order_release);
+        m_read.store(next(tail), memory_order_release);
         return true;
     }
 };
